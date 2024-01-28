0051c4ef346175ed8e6956807b4826e7278580e5
Taha Mulla <tahamulla@hotmail.com>
2024-01-28 15:34:14 -0500

    Added a function to get user info from DB

--- db/queries/users.js
+++ db/queries/users.js
@@ -37,5 +37,26 @@ const userExists = function (email) {
     });
 };
 
-module.exports = { getUsers, addUser, userExists };
+// To get user info by id
+const getUserById = function (userId) {
+  return new Promise((resolve, reject) => {
+
+    const queryStr = `
+    SELECT *
+    FROM users
+    WHERE id = $1;`;
+
+    const queryParams = [userId];
+
+    db.query(queryStr, queryParams)
+      .then(data => {
+        resolve(data.rows[0]);
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+};
+
+module.exports = { getUsers, addUser, userExists, getUserById };
 
