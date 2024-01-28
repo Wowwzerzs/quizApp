57b56b47c9551f1c76e16b24abce4a82cc3b8386
Taha Mulla <tahamulla@hotmail.com>
2024-01-28 15:49:07 -0500

    Added a function to display quizzes from DB

---
+++ db/queries/index.js
@@ -0,0 +1,17 @@
+const db = require('../connection');
+
+// display all public quizzes
+const getPublicQuizzes = function () {
+  return db.query(
+    `SELECT *
+    FROM quizzes
+    WHERE public = TRUE;`)
+    .then(data => {
+      return data.rows;
+    })
+    .catch(error => {
+      console.error(error);
+    });
+};
+
+module.exports = { getPublicQuizzes };
