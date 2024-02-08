cc8724b3dc8c20243c1d8941a531b5253e3ea410
Taha Mulla <tahamulla@hotmail.com>
2024-02-08 13:32:14 -0500

    Added a error route

---
+++ routes/error.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+// error page
+router.get('/error', (req, res) => {
+  try {
+    const errorMessage = decodeURIComponent(req.query.message);
+    res.render('error', { message: errorMessage });
+  } catch (err) {
+    console.error('Error rendering error page: ', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+module.exports = router;
